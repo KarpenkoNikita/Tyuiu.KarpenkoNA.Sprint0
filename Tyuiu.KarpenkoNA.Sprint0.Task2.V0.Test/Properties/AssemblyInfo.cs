10ae91c69bd51dacad0420782a78b43ef097aa9c
Tyuiu.KarpenkoNA@outlook.com <karpenko.nik05@mail.ru>
2023-09-12 18:56:49 +0500

    Task2

---
+++ Tyuiu.KarpenkoNA.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KarpenkoNA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KarpenkoNA.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("abe48235-0229-4d45-8580-0ed7306207fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KarpenkoNA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KarpenkoNA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KarpenkoNA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вызов класса DataService и метода GetVessage из библиотеки
+            Console.WriteLine(DataService.GetMessage("Никита"));
+            Console.ReadKey();
+        }
+    }
+}
